ff841a6654332e14145ff7de8adf51efb417a581
Pedro Augusto Cabral Oliveira <104571614+PedroAugusto2004@users.noreply.github.com>
2023-04-07 00:14:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ API_KEY = "815669c352784ab9a7588517e108fc8a"
 
 city_name = input("Digite o nome da cidade: ")
 
-link = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}"
+link = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&lang=pt_br"
 
 requisicao = requests.get(link)
 requisicao_dic= requisicao.json()
 descricao = requisicao_dic['weather'] [0] ['description']
 temperatura = requisicao_dic['main']['temp'] - 273.15
-print(descricao, temperatura)
+print(descricao, f"{temperatura}°C")
