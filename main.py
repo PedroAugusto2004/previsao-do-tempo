fbf179f861f2437c82fc0adb6f3cb1bf8c330587
Pedro Augusto Cabral Oliveira <104571614+PedroAugusto2004@users.noreply.github.com>
2023-04-16 16:05:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import requests
 
+
+from datetime import datetime
+
+agora = datetime.now()
+data_hora_formatada = agora.strftime("%d/%m/%Y %H:%M:%S")
+print("Data e hora:", data_hora_formatada)
+
 API_KEY = "815669c352784ab9a7588517e108fc8a"
 
 city_name = input("Digite o nome da cidade: ")
@@ -10,4 +17,5 @@ requisicao = requests.get(link)
 requisicao_dic= requisicao.json()
 descricao = requisicao_dic['weather'] [0] ['description']
 temperatura = requisicao_dic['main']['temp'] - 273.15
+temperatura= round(temperatura, 2)
 print(descricao, f"{temperatura}°C")
